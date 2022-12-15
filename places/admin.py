a0e7ec6af917e19fe4627e7633f83652bbe9bb67
Mark Ivchenko <marxygen@gmail.com>
2022-12-15 18:34:31 +0300

    Show picture preview

--- places/admin.py
+++ places/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 from places.models import Place, PlaceImage
+from django.utils.html import format_html
 
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
 
+    readonly_fields = ["image_preview"]
+    fields = ('title', 'image_preview', 'order')
+
+    def image_preview(self, obj):
+        return format_html('<img src="{url}" style="max-height: 200px; aspect-ratio={aspect_ratio}"/>'.format(
+            url=obj.image.url,
+            aspect_ratio=int(obj.image.width / obj.image.height)
+        )
+        )
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [PlaceImageInline]
-
-
-admin.site.register(PlaceImage)
