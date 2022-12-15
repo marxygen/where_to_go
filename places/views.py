f2fdcb837dbe1467a1577ad016c33f2543d8f39b
Mark Ivchenko <marxygen@gmail.com>
2022-12-15 18:04:02 +0300

    Change indentation

--- places/views.py
+++ places/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import get_object_or_404
 
 def retrieve_place(request, place_id: int):
     place = get_object_or_404(Place, id=place_id)
-    return JsonResponse(place.jsonify(), json_dumps_params={'ensure_ascii': False})
+    return JsonResponse(place.jsonify(), json_dumps_params={'ensure_ascii': False, 'indent': 2})
