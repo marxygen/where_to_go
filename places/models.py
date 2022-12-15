d968a2fa6f421de53c9f51279ec30cccc28d262e
Mark Ivchenko <marxygen@gmail.com>
2022-12-15 16:52:19 +0300

    Add Place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description_short = models.TextField(verbose_name='Короткое описание')
+    description_long = models.TextField(verbose_name='Длинное описание')
+    latitude = models.DecimalField(max_digits=22, decimal_places=16, verbose_name='Широта')
+    longitude = models.DecimalField(max_digits=22, decimal_places=16, verbose_name='Долгота')
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+
+    def __str__(self):
+        return self.title
