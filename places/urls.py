e022872b8ef53ee5e647144f664a62ac0bbec3a6
Mark Ivchenko <marxygen@gmail.com>
2023-01-30 00:26:47 +0500

    Remove hardcoded URLs

--- places/urls.py
+++ places/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from places.views import retrieve_place
 
-urlpatterns = [path("<int:place_id>/", retrieve_place)]
+urlpatterns = [path("<int:place_id>/", retrieve_place, name='retrieve_place')]
