9e7e48daa132eb75e517216dbf31ce4720d4301e
Mark Ivchenko <marxygen@gmail.com>
2023-02-06 21:49:01 +0500

    Remove unnecessary functions

--- places/views.py
+++ places/views.py
@@ -3,20 +3,15 @@ from places.models import Place
 from django.shortcuts import get_object_or_404
 
 
-def jsonify_place(place: Place) -> dict:
-    """Generate and return JSON representation"""
-    # Usually DRF's Serializer / ModelSerializer are used for this purpose
-    return {
-        "title": place.title,
-        "imgs": [img.image.url for img in place.images.all()],
-        "description_short": place.description_short,
-        "description_long": place.description_long,
-        "coordinates": {"lng": place.longitude, "lat": place.latitude},
-    }
-
-
 def retrieve_place(request, place_id: int):
     place = get_object_or_404(Place, id=place_id)
     return JsonResponse(
-        jsonify_place(place), json_dumps_params={"ensure_ascii": False, "indent": 2}
+        {
+            "title": place.title,
+            "imgs": [img.image.url for img in place.images.all()],
+            "description_short": place.description_short,
+            "description_long": place.description_long,
+            "coordinates": {"lng": place.longitude, "lat": place.latitude},
+        },
+        json_dumps_params={"ensure_ascii": False, "indent": 2},
     )
